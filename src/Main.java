eb51da0318d533efb0f80eebb8080b82a541725a
Viacheslav666 <cheep777@gmail.com>
2023-01-23 05:10:08 +0400

    easy

--- src/Main.java
+++ src/Main.java
@@ -5,29 +5,6 @@ public class Main {
 
 
     public static void main(String[] args) {
-        getAListOfAllEmployee();
-        totalSalary();
-        maxSalary();
-        minSalary();
-        printNameEmployee();
-        average();
-
-    }
-
-    public static void getAListOfAllEmployee() {
-        employee[0] = new Employee("Василий", 1, 150_000, Employee.counter);
-        employee[1] = new Employee("Кирилл", 2, 200, Employee.counter);
-        employee[2] = new Employee("Петя", 3, 20222, Employee.counter);
-        employee[3] = new Employee("Вася", 4, 20120, Employee.counter);
-        employee[4] = new Employee("Кирилл", 5, 20310, Employee.counter);
-        for (Employee employee : employee) {
-            System.out.println("el = " + employee);
-
-        }
-    }
-
-    public static int totalSalary() {
-
         employee[0] = new Employee("Василий", 1, 150_000, Employee.counter);
         employee[1] = new Employee("Кирилл", 2, 200, Employee.counter);
         employee[2] = new Employee("Петя", 3, 20222, Employee.counter);
@@ -38,6 +15,23 @@ public class Main {
         employee[7] = new Employee("Петя", 3, 20222, Employee.counter);
         employee[8] = new Employee("Вася", 4, 20120, Employee.counter);
         employee[9] = new Employee("Кирилл", 5, 20310, Employee.counter);
+        getAListOfAllEmployee();
+        totalSalary();
+        maxSalary();
+        minSalary();
+        printNameEmployee();
+        average();
+
+    }
+
+    public static void getAListOfAllEmployee() {
+        for (Employee employee : employee) {
+            System.out.println("el = " + employee);
+
+        }
+    }
+
+    public static int totalSalary() {
         int total = 0;
         int y = 0;
 
@@ -52,24 +46,12 @@ public class Main {
     }
 
     public static int maxSalary() {
-        employee[0] = new Employee("Василий", 1, 150_000, Employee.counter);
-        employee[1] = new Employee("Кирилл", 2, 200, Employee.counter);
-        employee[2] = new Employee("Петя", 3, 20222, Employee.counter);
-        employee[3] = new Employee("Вася", 4, 20120, Employee.counter);
-        employee[4] = new Employee("Кирилл", 5, 20310, Employee.counter);
-        employee[5] = new Employee("Василий", 1, 150_000, Employee.counter);
-        employee[6] = new Employee("Кирилл", 2, 200, Employee.counter);
-        employee[7] = new Employee("Петя", 3, 20222, Employee.counter);
-        employee[8] = new Employee("Вася", 4, 20120, Employee.counter);
-        employee[9] = new Employee("Кирилл", 5, 20310, Employee.counter);
         int max = employee[0].getSalary();
 
         for (int i = 0; i < employee.length; i++) {
             int salary = employee[i].getSalary();
             if (max < salary && employee != null) {
                 max = salary;
-
-
             }
         }
         System.out.println("max = " + max);
@@ -77,16 +59,6 @@ public class Main {
     }
 
     public static void minSalary() {
-        employee[0] = new Employee("Василий", 1, 150_000, Employee.counter);
-        employee[1] = new Employee("Кирилл", 2, 200, Employee.counter);
-        employee[2] = new Employee("Петя", 3, 20222, Employee.counter);
-        employee[3] = new Employee("Вася", 4, 20120, Employee.counter);
-        employee[4] = new Employee("Кирилл", 5, 20310, Employee.counter);
-        employee[5] = new Employee("Василий", 1, 150_000, Employee.counter);
-        employee[6] = new Employee("Кирилл", 2, 10, Employee.counter);
-        employee[7] = new Employee("Петя", 3, 20222, Employee.counter);
-        employee[8] = new Employee("Вася", 4, 20120, Employee.counter);
-        employee[9] = new Employee("Кирилл", 5, 20310, Employee.counter);
         int min = employee[0].getSalary();
         for (int i = 0; i < employee.length; i++) {
             int salary = employee[i].getSalary();
@@ -102,35 +74,13 @@ public class Main {
     }
 
     public static void printNameEmployee() {
-
-        employee[0] = new Employee("Василий", 1, 150_000, Employee.counter);
-        employee[1] = new Employee("Кирилл", 2, 200, Employee.counter);
-        employee[2] = new Employee("Петя", 3, 20222, Employee.counter);
-        employee[3] = new Employee("Вася", 4, 20120, Employee.counter);
-        employee[4] = new Employee("Кирилл", 5, 20310, Employee.counter);
-        employee[5] = new Employee("Василий", 1, 150_000, Employee.counter);
-        employee[6] = new Employee("Кирилл", 2, 10, Employee.counter);
-        employee[7] = new Employee("Петя", 3, 20222, Employee.counter);
-        employee[8] = new Employee("Вася", 4, 20120, Employee.counter);
-        employee[9] = new Employee("Кирилл", 5, 20310, Employee.counter);
-
         System.out.println("employee.toString() = " + employee.toString());
         for (int j = 0; j < employee.length; j++) {
             System.out.println("employee[i].getName() = " + employee[j].getName());
         }
     }
-    public static int average() {
 
-        employee[0] = new Employee("Василий", 1, 150_000, Employee.counter);
-        employee[1] = new Employee("Кирилл", 2, 200, Employee.counter);
-        employee[2] = new Employee("Петя", 3, 20222, Employee.counter);
-        employee[3] = new Employee("Вася", 4, 20120, Employee.counter);
-        employee[4] = new Employee("Кирилл", 5, 20310, Employee.counter);
-        employee[5] = new Employee("Василий", 1, 150_000, Employee.counter);
-        employee[6] = new Employee("Кирилл", 2, 200, Employee.counter);
-        employee[7] = new Employee("Петя", 3, 20222, Employee.counter);
-        employee[8] = new Employee("Вася", 4, 20120, Employee.counter);
-        employee[9] = new Employee("Кирилл", 5, 20310, Employee.counter);
+    public static int average() {
         int total = 0;
         int y = 0;
 
@@ -142,8 +92,7 @@ public class Main {
         System.out.println("average = " + average);
         return (int) average;
     }
-
-    }
+}
 
 
 
